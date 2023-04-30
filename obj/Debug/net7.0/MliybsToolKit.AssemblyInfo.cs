3b5f82290bb3d533af696519540785796c86f1b5
Mliybs <2491988463@qq.com>
2023-04-30 16:35:02 +0800

    update

--- obj/Debug/net7.0/MliybsToolKit.AssemblyInfo.cs
+++ obj/Debug/net7.0/MliybsToolKit.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mlinetles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Mliybs的工具包")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.9.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MliybsToolKit")]
